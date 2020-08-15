c7e3cb8f0215d01c150751a59c318616091c9423
Chatchai222 <65013350+Chatchai222@users.noreply.github.com>
2020-08-15 16:57:06 +0700

    Lab 2 HW 14 Aug 2020

---
+++ Lab 2 HW/main.py
@@ -0,0 +1,48 @@
+### This is a Direction Lab#2 14 Aug 2020 for Intro to Comp and Prog ###
+import math
+from turtle import *
+
+### Question 1 ###
+# w = 2
+# x = 3
+# y = 4
+# z = 5
+#
+# answer = x**3 + y * (z/2) - w
+# print(str(answer))
+
+### Question 2 ###
+# radius = float(input("Enter radius: "))
+# length = float(input("Enter length: "))
+# pi = math.pi
+#
+# area = pi * radius * radius
+# volume = area * length
+#
+# print("The area of cylinder is " + str(area))
+# print("The volume of cylinder is " + str(volume))
+
+### Question 3 ###
+# while True:
+#     showturtle()
+#     speed(1)
+#     left(60)
+#     forward(100)
+
+### Question 4 ###
+# x_coord = int(input("Enter x: "))
+# y_coord = int(input("Enter y: "))
+# radius = int(input("Enter radius: "))
+# while True:
+#     showturtle()
+#     up()
+#     goto(x_coord, y_coord - radius)
+#     down()
+#     pensize(1)
+#     speed(1)
+#     circle(radius)
+#     write(str(radius * radius * math.pi))
+
+
+
+
